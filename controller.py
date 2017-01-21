1817a04e39a33ce880c68e499f72a7f0836a39d4
David Whale <rackpi@thinkingbinaries.com>
2017-01-21 19:04:10 +0000

    Wrote a new controller app for micro:bit This supports up, down, left, right, A and B. It also has a flashing dot on the screen so you know it is working

---
+++ controller.py
@@ -0,0 +1,24 @@
+from microbit import *
+
+def loop():
+    x = accelerometer.get_x()
+    y = accelerometer.get_y()
+    a = button_a.is_pressed()
+    b = button_b.is_pressed()
+    
+    if a: print("A")
+    if b: print("B")
+    if x < -500: print("L")
+    if x > 500:  print("R")
+    if y < -500: print("D")
+    if y > 500:  print("U")
+
+def run():
+    while True:
+        display.set_pixel(2,2,9)
+        loop()
+        sleep(100)
+        display.set_pixel(2,2,0)
+        loop()
+        sleep(100)
+#run()
