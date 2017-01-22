fa228c28460d31b96c7d7c1d34afa6b745500218
David Whale <david@thinkingbinaries.com>
2017-01-22 14:40:32 +0000

    Still tinkering with controller. Might have to change this to get good latency/performance, especially if the game top loop runs at the same rate. i.e. may need to send all flags at once and parse them out so there is a single update per loop.

--- controller.py
+++ controller.py
@@ -1,13 +1,16 @@
 from microbit import *
 
+UPDATES_PER_SEC = 10
+RATE_MS         = 1000/UPDATES_PER_SEC
+
 def fly():
     x = accelerometer.get_x()
     y = accelerometer.get_y()
     a = button_a.is_pressed()
     b = button_b.is_pressed()
     
-    if a: print("A")
-    if b: print("B")
+    if a:        print("A")
+    if b:        print("B")
     if x < -500: print("L")
     if x > 500:  print("R")
     if y < -500: print("D")
@@ -17,8 +20,10 @@ def run():
     while True:
         display.set_pixel(2,2,9)
         fly()
-        sleep(100)
+        sleep(RATE_MS)
+
         display.set_pixel(2,2,0)
         fly()
-        sleep(100)
+        sleep(RATE_MS)
+
 run()
