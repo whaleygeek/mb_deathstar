4d2b2e5111ec83b5504f330155cf62474d7b7e6f
David Whale <david@thinkingbinaries.com>
2017-01-22 17:57:58 +0000

    Micro:bit controller working fine, performs really well

--- controller/device/mb_controller.py
+++ controller/device/mb_controller.py
@@ -1,29 +1,37 @@
 from microbit import *
 
 UPDATES_PER_SEC = 10
-RATE_MS         = 1000/UPDATES_PER_SEC
+DELAY_MS        = 1000/UPDATES_PER_SEC
 
 def fly():
     x = accelerometer.get_x()
     y = accelerometer.get_y()
     a = button_a.is_pressed()
     b = button_b.is_pressed()
-    
-    if a:        print("A")
-    if b:        print("B")
-    if x < -500: print("L")
-    if x > 500:  print("R")
-    if y < -500: print("D")
-    if y > 500:  print("U")
+
+    m = ""
+    if a:           m += 'A'
+    if b:           m += 'B'
+    if x < -500:    m += 'L'
+    if x > 500:     m += 'R'
+    if y < -500:    m += 'D'
+    if y > 500:     m += 'U'
+    if len(m) > 0:  print(m)
+
+brightness = 0
+def flash():
+    """flash the middle pixel of the display"""
+    global brightness
+    display.set_pixel(2, 2, brightness)
+    if brightness == 0:
+        brightness = 9
+    else:
+        brightness = 0
 
 def run():
     while True:
-        display.set_pixel(2,2,9)
         fly()
-        sleep(RATE_MS)
-
-        display.set_pixel(2,2,0)
-        fly()
-        sleep(RATE_MS)
+        flash()
+        sleep(DELAY_MS)
 
 run()
---
+++ controller/mb_controller.py
@@ -0,0 +1,28 @@
+# mb_controller.py - a simple controller module using serial polling of a micro:bit
+
+##import thread
+##import atexit
+import time
+import microbit # will auto-connect to micro:bit
+
+
+def get_command_flags():
+    """Get a collection of flags, one for each possible button/action"""
+
+    return microbit.read()
+
+
+#----- TEST HARNESS -----------------------------------------------------------
+
+if __name__ == "__main__": # test harness
+    while True:
+        flags = get_command_flags()
+        if flags is not None:
+            if 'U' in flags: print("up")
+            if 'D' in flags: print("down")
+            if 'L' in flags: print("left")
+            if 'R' in flags: print("right")
+            if 'A' in flags: print("fire")
+            if 'B' in flags: print("eject")
+
+# END
